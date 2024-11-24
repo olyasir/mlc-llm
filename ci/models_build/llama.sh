bc9e8d05bbb9d141e598fdeb7fd881590d59b892
olyasir <sirkinolya@gmail.com>
2024-11-24 11:03:52 +0000

    llama compilation workflow

---
+++ ci/models_build/llama.sh
@@ -0,0 +1,8 @@
+quantization="${1:-q0f32}"
+device="${2:-vulkan}"
+model=$3
+conv_template=$4
+
+python -m mlc_llm convert_weight --model-type llama "./$model/" --quantization "$quantization" --output "./output-$model-$quantization"
+python -m mlc_llm gen_config --model-type llama "./$model" --quantization "$quantization" --conv-template "conv_template" --output "./output-$model-$quantization"
+python -m mlc_llm compile --model-type llama "./output-$model-$quantization/" --quantization "$quantization" --device "$device" --output "./$model-$quantization.so"
